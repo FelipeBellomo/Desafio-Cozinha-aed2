795a64ef8d13746ae22e06f8ca1d70b92aa770e7
Felipe Bellomo <felipepereira1335@gmail.com>
2026-05-19 16:31:24 -0300

    feat: reading the JSON and creating a recipe object.

---
+++ system-cozinha/src/main/java/com/felipe/app/LeitorArquivos.java
@@ -0,0 +1,27 @@
+package com.felipe.app;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LeitorArquivos {
+
+    public List<Receita> carregarDados() {
+        Gson gson = new Gson();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("/home/felipe-bellomo/Documentos/aed-II/desafio-cozinha/system-cozinha/src/main/resources/pequenasReceitas.json"));
+            Receita[] listaReceitas = gson.fromJson(reader, Receita[].class);
+
+            return Arrays.asList(listaReceitas);
+
+        } catch (Exception e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+}
---
+++ system-cozinha/src/main/java/com/felipe/app/LivroDeReceitas.java
@@ -0,0 +1,11 @@
+package com.felipe.app;
+
+import java.util.List;
+
+public class LivroDeReceitas {
+
+    private int id;
+    private List<String> ingredientes;
+
+
+}
